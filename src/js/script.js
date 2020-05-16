8be618114071f9de6879464d13c6b05e3ee57885
Quazaru <qilaur@mail.ru>
2020-05-16 23:54:16 +0300

    Added new block with accordeons

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,24 @@
 import '../css/styles.scss';
 import 'normalize.css';
+const aboutAccordion = document.querySelectorAll('.about__button');
+
+
+
+
+const accordionToggle = () => {
+    ;
+}
+
+
+
+
+
+
+aboutAccordion.forEach( (item, i) => {
+    aboutAccordion[i].addEventListener('click', (event) => {
+        const target = event.target;
+        const accordion = target.closest('.about__accordion');
+        accordion.classList.toggle("about__accordion-active")
+    });
+    console.log(item, i , aboutAccordion);
+})
